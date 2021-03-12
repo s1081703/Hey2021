302a87da60428e85293d9fedfb5637f695718965
meio <meioshiorni@gmail.com>
2021-03-12 15:32:45 +0800

    合併後，再次修改txv顯示的文字

--- app/src/main/java/com/example/hey2021/MainActivity.kt
+++ app/src/main/java/com/example/hey2021/MainActivity.kt
@@ -9,19 +9,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello Hey"
-        TestMaster()
-    }
-    fun TestMaster(){
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Master分支"
-    }
-        TestDEV()
+        //txv.text = "Hello Hey"
+        //TestDEV()
+        //TestMaster()
+        txv.setText("嗨，我是YJY");
     }
     fun TestDEV(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Dev分支"
     }
-
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
     }
+
 }
