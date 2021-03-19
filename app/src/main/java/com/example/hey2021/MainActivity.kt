281bb72fc62392a4471bf30fa634c1921ee7295a
student <s1081703@gm.pu.edu.tw>
2021-03-19 13:49:11 +0800

    他人加入螢幕觸控方法

--- app/src/main/java/com/example/hey2021/MainActivity.kt
+++ app/src/main/java/com/example/hey2021/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hey2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +23,10 @@ class MainActivity : AppCompatActivity() {
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Master分支"
     }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "tcyangpu修改螢幕觸控"
+        return true
 
-}
+
+    }
